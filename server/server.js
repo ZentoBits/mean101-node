25c7623d208a212090984766a8158ded21f14b77
Zachary George <zacharygeorge@Zacharys-MacBook-Pro.local>
2016-09-26 13:51:17 -0500

    able to post

--- server/server.js
+++ server/server.js
@@ -1,11 +1,40 @@
 'use strict'
 
+const { json } = require('body-parser')
 const express = require('express')
-const app = express()
+const mongoose = require('mongoose')
 
-const port = process.env.PORT || 3000
-app.set('port', port)
+const app = express()
+const MONGODB_URL = process.env.MONGODB_URL || 'mongodb://localhost:27017/meanChat'
+const PORT = process.env.PORT || 3000
 
 app.use(express.static('client'))
+app.use(json())
 
-app.listen(port, () => console.log(`Listening on port: ${port}`))
+app.get('/api/title', (req, res) =>
+  res.json({ title: 'MEAN Chat' })
+)
+
+const Message = mongoose.model('message', {
+  author: String,
+  content: String,
+})
+
+app.get('/api/messages', (req, res, err) =>
+  Message
+    .find()
+    .then(messages => res.json({ messages }))
+    .catch(err)
+)
+
+app.post('/api/messages', (req, res, err) =>
+  Message
+    .create(req.body)
+    .then(msg => res.json(msg))
+    .catch(err)
+)
+
+mongoose.Promise = Promise
+mongoose.connect(MONGODB_URL, () =>
+  app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
+)
